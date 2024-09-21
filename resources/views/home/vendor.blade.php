c9e5a50371d09f33c4899ed551a4fbcd7821d926
Smiljanic19A <aleksandar.smiljanic19@gmail.com>
2024-09-21 13:05:00 +0200

    added all the setup blades

--- resources/views/home/vendor.blade.php
+++ resources/views/home/vendor.blade.php
@@ -8,4 +8,29 @@
     @else
         @dd("kurac")
     @endif
+
+    <!-- Page is used for setting up vendor information before they are publicly viewed -->
+    <h1>Setup You Vendor Profile!te</h1>
+    <div class="setup-container">
+        <!-- Services -->
+        <div class="setup_field">
+            Privacy
+        </div>
+        <div class="setup_field">
+            Services
+        </div>
+        <!-- Schedule -->
+        <div class="setup_field">
+            Schedule
+        </div>
+        <!-- Design -->
+        <div class="setup_field">
+            Design
+        </div>
+        <!-- Settings -->
+        <div class="setup_field">
+            Settings
+        </div>
+    </div>
+
 @endsection
